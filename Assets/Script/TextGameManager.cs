05144c413448f4ec15eb7f2790b0d037f26d2047
eternal66699 <julian.lopez@ciit.edu.ph>
2023-06-06 14:32:48 +0800

    l

--- Assets/Script/TextGameManager.cs
+++ Assets/Script/TextGameManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextGameManager : MonoBehaviour
 {
+    public TextMeshProUGUI
+        storyTextMeshPro, hpTextMeshPro, staminaTextMeshPro;
+    public string storyText;
+    public int hpValue;
+    public int staminaValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        storyTextMeshPro.text = storyText; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        storyTextMeshPro.text = storyText;
+        storyText = "";
+        hpTextMeshPro.text = hpValue.ToString();
+        staminaTextMeshPro.text = staminaValue.ToString();
     }
 }
